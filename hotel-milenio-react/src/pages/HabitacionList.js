2f35b474ce693bc3fc41d06aba17375e02101add
MarceloMartinez7 <mr5405750@gmail.com>
2023-10-25 11:14:48 -0600

    Completo el actualizar Habitaciones

--- hotel-milenio-react/src/pages/HabitacionList.js
+++ hotel-milenio-react/src/pages/HabitacionList.js
@@ -46,10 +46,11 @@ function HabitacionList() {
       .catch((error) => console.error('Error al obtener las habitaciones:', error));
   };
 
+
   // Función para enviar el formulario de actualización
   const handleUpdate = () => {
     // Realiza una solicitud PUT al servidor para actualizar el registro de la habitación
-    fetch(`http://localhost:5000/crud/updateHabitacion/${selectedHabitacion.ID_Habitacion}`, {
+    fetch(`http://localhost:5000/crud/habitacion/update/${selectedHabitacion.ID_Habitacion}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -65,34 +66,6 @@ function HabitacionList() {
       })
       .catch((error) => console.error('Error al actualizar el registro de la habitación:', error));
   };
-
-
-/*   const handleDeleteHabitacion = (idHabitacion) => {
-    const confirmation = window.confirm('¿Seguro que deseas eliminar esta habitación?');
-    if (confirmation) {
-      // Realiza una solicitud DELETE al servidor para eliminar la habitación
-      fetch(`http://localhost:5000/crud/habitacion/delete/${idHabitacion}`, {
-        method: 'DELETE',
-      })
-        .then((response) => {
-          if (response.ok) {
-            // La eliminación fue exitosa, puedes realizar alguna acción adicional si lo deseas
-            alert('Habitación eliminada con éxito.');
-          } else {
-            alert('Error al eliminar la habitación. Por favor, inténtalo de nuevo más tarde.');
-          }
-        })
-        .catch((error) => {
-          console.error('Error al eliminar la habitación:', error);
-          alert('Ocurrió un error al eliminar la habitación. Por favor, verifica tu conexión a Internet o inténtalo de nuevo más tarde.');
-        });
-    }
-  }; */
-
-
-
-
-
 
 
   // Función para manejar la eliminación de una habitación
@@ -136,7 +109,7 @@ function HabitacionList() {
               <tr>
                 <th>ID</th>
                 <th>Número de Habitación</th>
-                <th>ID Tipo de Habitación</th>
+                <th>Tipo de Habitación</th>
                 <th>Número de Camas</th>
                 <th>ID Estado</th>
                 <th>Precio</th>
@@ -163,8 +136,89 @@ function HabitacionList() {
       </Card>
 
       <Modal show={showModal} onHide={() => setShowModal(false)} size="lg">
-        {/* ... Código para el modal de edición de habitaciones */}
-      </Modal>
+  <Modal.Header closeButton>
+    <Modal.Title>Actualizar Habitación</Modal.Title>
+  </Modal.Header>
+  <Modal.Body>
+    <Card className="mt-3">
+      <Card.Body>
+        <Card.Title>Registro de Habitación</Card.Title>
+        <Form className="mt-3">
+          <Row className="g-3">
+            <Col sm="6" md="6" lg="4">
+              <FloatingLabel controlId="numeroHabitacion" label="Número de Habitación">
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese el número de habitación"
+                  name="N_de_habitacion"
+                  value={formData.N_de_habitacion}
+                  onChange={handleFormChange}
+                />
+              </FloatingLabel>
+            </Col>
+
+            <Col sm="6" md="6" lg="4">
+              <FloatingLabel controlId="tipoHabitacion" label="ID Tipo de Habitación">
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese el ID del tipo de habitación"
+                  name="ID_tipoHabitacion"
+                  value={formData.ID_tipoHabitacion}
+                  onChange={handleFormChange}
+                />
+              </FloatingLabel>
+            </Col>
+
+            <Col sm="6" md="6" lg="4">
+              <FloatingLabel controlId="numeroCamas" label="Número de Camas">
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese el número de camas"
+                  name="Num_Cama"
+                  value={formData.Num_Cama}
+                  onChange={handleFormChange}
+                />
+              </FloatingLabel>
+            </Col>
+
+            <Col sm="6" md="6" lg="4">
+              <FloatingLabel controlId="idEstado" label="ID Estado">
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese el ID del estado"
+                  name="ID_Estado"
+                  value={formData.ID_Estado}
+                  onChange={handleFormChange}
+                />
+              </FloatingLabel>
+            </Col>
+
+            <Col sm="6" md="6" lg="4">
+              <FloatingLabel controlId="precio" label="Precio">
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese el precio"
+                  name="Precio"
+                  value={formData.Precio}
+                  onChange={handleFormChange}
+                />
+              </FloatingLabel>
+            </Col>
+          </Row>
+        </Form>
+      </Card.Body>
+    </Card>
+  </Modal.Body>
+  <Modal.Footer>
+    <Button variant="secondary" onClick={() => setShowModal(false)}>
+      Cerrar
+    </Button>
+    <Button variant="primary" onClick={handleUpdate}>
+      Actualizar
+    </Button>
+  </Modal.Footer>
+</Modal>
+
     </div>
   );
 }
